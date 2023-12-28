431a8eceb416ad71b45de6301f2f0d3effeb538b
Dávid János Fekete <janosdavidfekete@gmail.com>
2023-12-28 18:19:54 +0100

    Language chooser removed, game status checker created

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
+import os
 class App:
     def __init__(self):
         self.firstsave = 'firstsave.txt'
-    
-    def file_reading(self):
-        pass
-    
-    def new_game(self):
-        fp = open(self.firstsave, 'r+', encoding='utf-8')
+        self.secondsave = 'secondsave.txt'
+        self.thirdsave = 'thirdsave.txt'
+        self.option = ''
+
+    def intro(self):
+        print('---------------------------------')
+        print('----------Soccer Career----------')
+        print('---------------------------------')
+        print('Created by: VhoiX\n\nIf you choose one option just write what it in between the (). Have fun!\n\n')
         
-    
-    def load_game(self):
-        fp = open(self.firstsave, 'w+', encoding='utf-8')
-        lines = fp.readlines()
-        if len(lines) < 0:
-            print('1. -- játék betöltése (elso)')
-        else:
-            print('1. -- nincs mentett játék')
+
+    #Here the program checks if there is a saved game or not.
+    def check_saved_games(self):
+        try:
+            fp = open(self.firstsave, 'r', encoding='utf-8')
+            fp.close()
+            print('Continue first game (first)')
+            
+            fp = open(self.secondsave, 'r', encoding='utf-8')
+            fp.close()
+            print('Continue second game (second)')
+            
+            fp = open(self.thirdsave, 'r', encoding='utf-8')
+            fp.close()
+            print('Continue third game (third)')
+            
+        except FileNotFoundError:
+            print('New game (new)')
         
-    #Amíg nincs angol verzió addig ez a része a programnak felesleges.
-    def language_chooser(self):
-        print('-------------------------------------------')
-        language = input('Choose language:\n Hungarian\n English\nChoosen Option: ')
-        if language == 'Hungarian':
-            print('--------------------------------')
-            print('Üdvözöllek a játékban!')
-        elif language == 'English':
-            print('--------------------------------')
-            print('Welcome in to the game!')
-            print('Unfortunately the English language is not available at the moment. ')
+        while True:
+            self.option = input('Choosen option:')
+            if self.option == 'new' or self.option == 'first' or self.option == 'second' or self.option == 'third':
+                break 
     
     def run_game(self):
-        # app.language_chooser()
-        app.load_game()
-
+        app.intro()
+        app.check_saved_games()
+        
         
 app = App()
 app.run_game()
