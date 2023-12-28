a8aef094d3eb525e6ee6bdd24514c61d2fb793f0
Dávid János Fekete <janosdavidfekete@gmail.com>
2023-12-28 10:34:40 +0100

    starting phase

---
+++ main.py
@@ -0,0 +1,17 @@
+class App:
+    def __init__(self):
+        pass
+    
+    def file_reading(self):
+        pass
+    
+    def language_chooser(self):
+        print('-------------------------------------------')
+        language = input('Choose language:\n Hungarian\n English\nChoosen Option: ')
+    
+    def run_game(self):
+        app.language_chooser()
+        
+        
+app = App()
+app.run_game()
